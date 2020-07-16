be19b82828ae1bcddaef87f5a9a36c5459bd3e96
Emanuel Betcel <emanuelbetcel@gmail.com>
2020-07-16 01:49:57 -0300

    Finished part 2 function complete()

--- airpol.R
+++ airpol.R
@@ -1,16 +1,33 @@
-
 #Calculates the mean of polutant
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     
     #Get all the files names from selected IDs
     files <- list.files(directory)[id]
-    #Creating DataFrame
+    #Create DataFrame
     df <- data.frame()
     #For loop to read all .csv files with selected id and bind to df 
     for(i in files){
         df <-rbind(df, read.csv(paste0(getwd(),"/",directory,"/",i)))
     }
     #Returns the mean of pollutant column
-    mean(df[,pollutant], na.rm = TRUE)
+    return(mean(df[,pollutant], na.rm = TRUE))
 }
+
+complete <- function(directory, id = 1:332){
+    #Gets all the files names and saves to a list
+    files <- list.files(directory)
+    #Creates DataFrame
+    df <- data.frame()
+    for (i in id){
+        #Reads files
+        data <- read.csv(paste0(getwd(),"/",directory,"/",files[i]))
+        #Sum all non null observations
+        nobs <- sum(complete.cases(data)) 
+        df <- rbind(df, data.frame(i,nobs))
+    }
+    #Set dataframe column names
+    colnames(df) <- c("id", "nobs")
+    return(df)
+}
+    
 
